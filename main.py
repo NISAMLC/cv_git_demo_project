2697d23638a3d299ea38a1bd07a82a8a425a68b6
Rashid <muhammedrashid7274@gmail.com>
2024-06-12 16:11:42 +0530

    added text on image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 img = cv2.imread('redd.png')
 cv2.line(img,(0,0),(512,512),color=(0,0,255),thickness=3)
+cv2.putText(img,("Its lana's Image Right?"),(100,200),fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(0,250,200),thickness=3)
 cv2.imshow("img",img)
 cv2.waitKey(0)
