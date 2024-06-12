49315fbb0468a6f2da40887f9f2a77e25d84f256
NISAMLC <nisamlckkn@gmail.com>
2024-06-12 15:51:08 +0530

    milan added a new line on top of the image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 img = cv2.imread('redd.png')
 cv2.line(img,(0,0),(512,512),color=(0,0,255),thickness=3)
+cv2.line(img,(0,512),(512,0),color=(255,0,255),thickness=3)
 cv2.imshow("img",img)
 cv2.waitKey(0)
