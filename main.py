d65aa4cc3df88cae86ce64be68fafc6ca68d36b7
Milan Chand <mchandmilan@gmail.com>
2024-06-12 16:16:12 +0530

    milan added a new line

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import cv2
 img = cv2.imread('redd.png')
 cv2.line(img,(0,0),(512,512),color=(0,0,255),thickness=3)
+##new_intro
+cv2.line(img,(256,0),(256,512),color=(255,255,255),thickness=4)
+##
 cv2.imshow("img",img)
 cv2.waitKey(0)
